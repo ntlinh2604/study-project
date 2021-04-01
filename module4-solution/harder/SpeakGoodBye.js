7ed89460e94bfcde08b495368d36e923013725f0
ntlinh2604 <ntlinh2604@gmail.com>
2021-04-01 10:25:10 +0200

    module4-solution

---
+++ module4-solution/harder/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(){
+    var byeSpeaker={};
+    var speakWord="Good Bye";
+    byeSpeaker.speak=function speak(names) {
+        console.log(speakWord + " " + names);
+      }
+    window.byeSpeaker=byeSpeaker
+})();
